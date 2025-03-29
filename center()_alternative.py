42259ad3f4a0e0977b7357884fef8d631d914364
Carlos De Guzman <raremagic01@gmail.com>
2025-03-29 11:22:27 +0800

    print word in middle

--- center()_alternative.py
+++ center()_alternative.py
@@ -1,4 +1,8 @@
 #input string
+text = input("Enter a string: ")
+width = int(input("Enter the width: "))
 #input width
 
 #print output
+print("Output: ", text.rjust((width + len(text)) // 2).ljust(width))
+ 
