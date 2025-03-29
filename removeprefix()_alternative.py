67f57199df2dc2465cff814f82810ba1b7e40f78
Carlos De Guzman <raremagic01@gmail.com>
2025-03-29 10:00:47 +0800

    remove parts of words

--- removeprefix()_alternative.py
+++ removeprefix()_alternative.py
@@ -1,6 +1,11 @@
 #input string
+text = input("Enter a string: ")
+prefix = input("Enter prefix to remove: ")
 #input prefix to remove
 
 #set rules
+if text.startswith(prefix):
+    text = text[len(prefix):]
 
 #print output
+print("Output: ", text)
