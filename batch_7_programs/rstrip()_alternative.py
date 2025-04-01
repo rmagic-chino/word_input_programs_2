f54b0ffb046ac02f4ca026f56622fb93976da7d1
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 07:35:14 +0800

    print without spaces on right

--- batch_7_programs/rstrip()_alternative.py
+++ batch_7_programs/rstrip()_alternative.py
@@ -1,4 +1,10 @@
 #input string
+user_input = input("Enter a string: ")
+index = len(user_input) - 1
+
 #set condiitons
+while index >= 0 and user_input[index] == ' ':
+    index -= 1
 
 #print output
+print("Trimmed string:", user_input[:index + 1])
