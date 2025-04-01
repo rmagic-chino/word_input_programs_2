eaea272b63b04cb13295c4a00a013c0837ea30e7
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 07:40:50 +0800

    print final string w/o suffix

--- batch_7_programs/removesuffix()_alternative.py
+++ batch_7_programs/removesuffix()_alternative.py
@@ -1,6 +1,11 @@
 #input string
+user_input = input("Enter a string: ")
+suffix = input("Enter a suffix to remove: ")
 #input suffix to remove
 
 #set conditions
+if user_input.endswith(suffix):
+    user_input = user_input[:len(user_input) - len(suffix)]
 
 #print output
+print("Trimmed string:", user_input)
