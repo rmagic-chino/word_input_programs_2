ffcd9c42068a83b9ef854e4fadb58b9d0994609b
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 07:54:28 +0800

    print in upper case

--- batch_7_programs/upper()_alternative.py
+++ batch_7_programs/upper()_alternative.py
@@ -1,5 +1,13 @@
 #input string
+user_input = input("Enter a string: ")
+uppercase_str = ""
 
 #construct loop
+for char in user_input:
+    if 'a' <= char <= 'z':
+        uppercase_str += chr(ord(char) - 32)
+    else:
+        uppercase_str += char
 
 #print output
+print("Uppercase string:", uppercase_str)
