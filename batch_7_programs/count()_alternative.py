7a2ad02114ca103e919aa7f34ad1e6216c274cdf
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 10:51:54 +0800

    count duplicates of words

--- batch_7_programs/count()_alternative.py
+++ batch_7_programs/count()_alternative.py
@@ -1,6 +1,13 @@
 #input string
+user_input = input("Enter a string: ")
+substring = input("Enter a substring to check: ")
 #input substring to check
 
 #set rules and conditions
+count, index = 0, 0
+while (index := user_input.find(substring, index)) != -1:
+    count += 1
+    index += len(substring)
 
 #print output
+print("Count of substring:", count)
