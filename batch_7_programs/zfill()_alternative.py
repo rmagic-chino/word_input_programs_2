133dfdb7f333cc9385d7c4eba92d58201c4196f3
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 10:36:35 +0800

    fill blanks in length with 0s

--- batch_7_programs/zfill()_alternative.py
+++ batch_7_programs/zfill()_alternative.py
@@ -1,6 +1,10 @@
 #input string
+user_input = input("Enter a string: ")
+total_length = int(input("Enter the total length: "))
 #input length
 
 #set conditions
+padded_str = '0' * (total_length - len(user_input)) + user_input
 
 #print output
+print("Padded string: ", padded_str)
