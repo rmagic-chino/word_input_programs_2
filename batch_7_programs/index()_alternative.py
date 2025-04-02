4598947ace40b2976608a49c14c87c167f1519f2
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 10:57:49 +0800

    print index

--- batch_7_programs/index()_alternative.py
+++ batch_7_programs/index()_alternative.py
@@ -1,6 +1,14 @@
 #input string
+user_input = input("Enter a string: ")
+substring = input("Enter a substring to find: ")
 #input substring to find
 
 #set conditions
+index = -1
+for i in range(len(user_input) - len(substring) + 1):
+    if user_input[i:i + len(substring)] == substring:
+        index = i
+        break
 
 #print output
+print("First index of substring:", index)
