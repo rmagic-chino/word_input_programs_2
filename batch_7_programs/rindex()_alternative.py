cce9288ee1bdf985f4b6ec022b4c798d8e4f15bc
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 11:04:19 +0800

    print last index

--- batch_7_programs/rindex()_alternative.py
+++ batch_7_programs/rindex()_alternative.py
@@ -1,5 +1,13 @@
 #input string
+user_input = input("Enter a string: ")
+substring = input("Enter a substring to check: ")
 
 #set conditions
+index = -1
+for i in range(len(user_input) - len(substring) -1, -1):
+    if user_input[i:i + len(substring)] == substring:
+        index = i
+        break
 
 #print output
+print("Last index of substring:", index)
