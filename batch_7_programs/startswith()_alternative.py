04e677ce19a249bfeec213d5b4dc4cdd4e26d1b3
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 10:07:21 +0800

    print true if prefix match

--- batch_7_programs/startswith()_alternative.py
+++ batch_7_programs/startswith()_alternative.py
@@ -1,5 +1,9 @@
 #input string
+user_input = input("Enter a string: ")
+prefix = input("Enter a prefix to check: ")
 
 #set rules
+matches = user_input[:len(prefix)] == prefix
 
 #print output
+print("Starts with prefix:", matches)
