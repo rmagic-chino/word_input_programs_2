da6f096c648abb3d52ab6ec29ff4be3e947e72b1
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 10:19:05 +0800

    print in middle of length

--- batch_7_programs/rjust()_alternative.py
+++ batch_7_programs/rjust()_alternative.py
@@ -1,10 +1,9 @@
 #input string
 user_input = input("Enter a string: ")
-total_length = input("Enter the total length: "))
-#input length
+total_length = int(input("Enter the total length: "))
 
 #set conditions
 padded_str = ' ' * (total_length - len(user_input)) + user_input
 
 #print output
-print("padded string:", padded_str)
+print("padded string: ", padded_str)
