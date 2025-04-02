a412c460ac3222b4aeab774746187878fdc242b2
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 09:44:41 +0800

    print true if lowercase

--- batch_7_programs/islower()_alternative.py
+++ batch_7_programs/islower()_alternative.py
@@ -1,5 +1,12 @@
 #input string
+user_input = input("Enter a string: ")
+all_lowercase = True
 
 #construct loop
+for char in user_input:
+    if not ('a' <= char <= 'z'):
+        all_lowercase = False
+        break
 
 #print output
+print("All lowercase:", all_lowercase)
