e7d2798e8b79adde99aaee2b8ccef434373bcc72
Carlos De Guzman <raremagic01@gmail.com>
2025-04-02 10:16:13 +0800

    print with desired length

--- batch_7_programs/rjust()_alternative.py
+++ batch_7_programs/rjust()_alternative.py
@@ -1,6 +1,10 @@
 #input string
+user_input = input("Enter a string: ")
+total_length = input("Enter the total length: "))
 #input length
 
 #set conditions
+padded_str = ' ' * (total_length - len(user_input)) + user_input
 
 #print output
+print("padded string:", padded_str)
